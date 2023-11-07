db3222f28b9cb264821fc0fe624e69ccbd339029
IgorDoofeev <gothar2010@yandex.ru>
2023-11-07 22:38:24 +0300

    lesson OOP update

---
+++ OOP/descriptor.py
@@ -0,0 +1,42 @@
+class ReadInX:
+    def __set_name__(self, owner, name):
+        self.name = '_x'
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+
+class Integer:
+    @classmethod
+    def verify_coords(cls, coord):
+        if type(coord) != int:
+            raise TypeError('Координаты должны быть целым числом')
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        #return instance.__dict__[self.name]
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        self.verify_coords(value)
+        #instance.__dict__[self.name] = value
+        setattr(instance, self.name, value)
+
+
+class Point3D:
+    x = Integer()
+    y = Integer()
+    z = Integer()
+    xr = ReadInX()
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+p = Point3D(1, 2, 3)
+p.xr = 5
+print(p.xr, p.__dict__)
